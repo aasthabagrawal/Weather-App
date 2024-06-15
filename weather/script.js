ba4f666cc48da01ee38b50bfe70fbd29f0fd344c
Aastha Agrawal <114677021+aasthabagrawal@users.noreply.github.com>
2024-06-15 20:12:00 +0530

    Add files via upload

---
+++ weather/script.js
@@ -0,0 +1,51 @@
+const options = {
+    method: 'GET',
+    headers: {
+        'x-rapidapi-key': 'd53a30c2d1msh3800f22f8aeb495p1bf04djsn7ac813f8b66f',
+        'x-rapidapi-host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
+};
+
+const fetchWeather = async (city) => {
+    const url = `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`;
+    try {
+        cityName.innerHTML = city;
+        const response = await fetch(url, options);
+        const result = await response.json(); // Parse the response as JSON
+        console.log(result);
+
+        // Assuming you have elements with the following IDs in your HTML
+        document.getElementById('cloud_pct').innerHTML = result.cloud_pct;
+        document.getElementById('temp').innerHTML = result.temp;
+        document.getElementById('temp2').innerHTML = result.temp;
+        document.getElementById('feels_like').innerHTML = result.feels_like;
+        document.getElementById('humidity').innerHTML = result.humidity;
+        document.getElementById('humidity2').innerHTML = result.humidity;
+        document.getElementById('min_temp').innerHTML = result.min_temp;
+        document.getElementById('max_temp').innerHTML = result.max_temp;
+        document.getElementById('wind_speed').innerHTML = result.wind_speed;
+        document.getElementById('wind_speed2').innerHTML = result.wind_speed;
+        document.getElementById('wind_degrees').innerHTML = result.wind_degrees;
+        document.getElementById('sunrise').innerHTML = result.sunrise;
+        document.getElementById('sunset').innerHTML = result.sunset;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+// Initial fetch for default city
+
+
+// Add event listener for the form submission
+submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    const city = document.getElementById('city').value;
+    fetchWeather(city);
+});
+
+fetchWeather('Delhi');
+
+
+      
+
+    
